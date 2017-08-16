2aaa05c194fdd661cafb914831bd85354d3c7c7f
George Rainone <georgera@co.dac.int>
2017-08-16 08:19:46 -0600

    GH60 Mouseing

---
+++ keyboards/satan/keymaps/geo/config.h
@@ -0,0 +1,13 @@
+#include "../../config.h"
+
+/* #define BACKLIGHT_BREATHING
+#define MOUSEKEY_DELAY             32
+#define MOUSEKEY_INTERVAL          32
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       10 */
+
+#define BACKLIGHT_BREATHING
+#define MOUSEKEY_DELAY             16
+#define MOUSEKEY_INTERVAL          16
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_TIME_TO_MAX       20
