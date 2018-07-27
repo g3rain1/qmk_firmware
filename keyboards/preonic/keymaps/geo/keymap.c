4149a9a1abc842d788d7989ed155bfba040acd76
George Rainone <g3rain1@hotmail.com>
2018-07-27 12:24:44 -0600

    Fixed [] key placement

--- keyboards/preonic/keymaps/geo/keymap.c
+++ keyboards/preonic/keymaps/geo/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL},
         {LT(_FL, KC_F2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT},
         {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT},
-        {KC_LCTL, KC_LGUI, KC_LALT, KC_LBRC, KC_RBRC, KC_SPC, TD(DANCE_LAYERS), KC_MINS, KC_EQL, KC_QUOT, KC_BSLS, KC_ESC}},
+        {KC_LCTL, KC_LGUI, KC_LALT, KC_MINS, KC_EQL, KC_SPC, TD(DANCE_LAYERS), KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_ESC}},
 
     [_FL] =
         {{KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11},
@@ -132,25 +132,12 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) //
   }
   return MACRO_NONE;
 };
-#define NOTE_CLICK 18000.0f
-float tone_click[][2] = SONG(MUSICAL_NOTE(_CLICK,1));
-
-uint8_t *click_sample;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
   if (!process_record_dynamic_macro(keycode, record))
   {
     return false;
-  }
-  if (record->event.pressed)
-  {
-    PLAY_SONG(tone_click);
-    //play_sample(click_sample, 10, true);
-  }
-  else
-  {
-    //stop_all_notes();
   }
   return true;
 }
